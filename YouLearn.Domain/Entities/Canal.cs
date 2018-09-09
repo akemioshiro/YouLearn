5526c7d7d7c7326eb62bafb1e419f2d642145b6e
patricia.oshiro@totvs.com.br <patricia.oshiro@totvs.com.br>
2018-09-09 02:17:59 -0300

    Criação de entidades

---
+++ YouLearn.Domain/Entities/Canal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace YouLearn.Domain.Entities
+{
+    public class Canal
+    {
+        public string Nome { get; set; }
+        public string UrlLogo { get; set; }
+        public Usuario Usuario { get; set; }
+        public Guid Id { get; set; }
+
+    }
+}
---
+++ YouLearn.Domain/Entities/Playlist.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace YouLearn.Domain.Entities
+{
+    public class Playlist
+    {
+        public Guid Id { get; set; }
+
+        public Usuario Usuario { get; set; }
+
+        public string Status { get; set; }
+    }
+}

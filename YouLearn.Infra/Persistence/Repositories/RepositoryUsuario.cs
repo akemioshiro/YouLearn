3ae7a4d77d1fae7d3a289665423865807509d45c
patricia.oshiro@totvs.com.br <patricia.oshiro@totvs.com.br>
2018-09-09 20:25:16 -0300

    metodos do repositorio de usuario

--- YouLearn.Infra/Persistence/Repositories/RepositoryUsuario.cs
+++ YouLearn.Infra/Persistence/Repositories/RepositoryUsuario.cs
@@ -1,29 +1,38 @@
 ﻿using System;
+using System.Linq;
 using YouLearn.Domain.Entities;
 using YouLearn.Domain.Interfaces.Repositories;
+using YouLearn.Infra.Persistence.EF;
 
 namespace YouLearn.Infra.Persistence.Repositories
 {
     public class RepositoryUsuario : IRepositoryUsuario
     {
+        private readonly YouLearnContext _context;
+
+        public RepositoryUsuario(YouLearnContext context)
+        {
+            _context = context;
+        }
+
         public bool Existe(string email)
         {
-            throw new NotImplementedException();
+            return _context.Usuarios.Any(x=>x.Email.Endereco==email);
         }
 
         public Usuario Obter(Guid id)
         {
-            throw new NotImplementedException();
+            return _context.Usuarios.FirstOrDefault(x => x.Id == id);
         }
 
         public Usuario Obter(string email, string senha)
         {
-            throw new NotImplementedException();
+            return _context.Usuarios.FirstOrDefault(x => x.Email.Endereco == email && x.Senha==senha);
         }
 
         public void Salvar(Usuario usuario)
         {
-            throw new NotImplementedException();
+            _context.Usuarios.Add(usuario);
         }
     }
 }
